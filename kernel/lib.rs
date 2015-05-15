7f8a8751e479ca3d876ac7742d5e0798629cd5ca
Mark Mansi <mmansi@utexas.edu>
2015-05-15 12:15:36 -0500

    Painting screen

--- kernel/lib.rs
+++ kernel/lib.rs
@@ -4,17 +4,53 @@
 #![crate_type = "staticlib"]
 #![crate_name = "rustcode"]
 
+#![allow(dead_code)]
+
 #[macro_use]
 extern crate core;
 extern crate rlibc;
 
+use core::prelude::*;
 use core::fmt;
 
-#[no_mangle]
-pub fn kernel_main() {
-
+enum Color {
+    Black      = 0,
+    Blue       = 1,
+    Green      = 2,
+    Cyan       = 3,
+    Red        = 4,
+    Pink       = 5,
+    Brown      = 6,
+    LightGray  = 7,
+    DarkGray   = 8,
+    LightBlue  = 9,
+    LightGreen = 10,
+    LightCyan  = 11,
+    LightRed   = 12,
+    LightPink  = 13,
+    Yellow     = 14,
+    White      = 15,
 }
 
+fn clear_screen(background: u16) {
+    for i in 0..(80 * 25) {
+        unsafe {
+            *((0xb8000 + i * 2) as *mut u16) = background << 12;
+        }
+    }
+}
+
+/**
+ * This is the entry point to the kernel. It is the first rust code that runs.
+ */
+#[no_mangle]
+pub fn kernel_main() {
+    let color = Color::LightGreen as u16;
+    clear_screen(color);
+}
+
+
+// For bare-bones rust
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
 #[lang = "eh_personality"] extern fn eh_personality() {}
 
