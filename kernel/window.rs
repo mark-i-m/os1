63429d5fd08e38fa92c594921484a1cdb4afb6be
Mark Mansi <mmansi@utexas.edu>
2015-05-18 12:13:40 -0500

    Removed comment

--- kernel/window.rs
+++ kernel/window.rs
@@ -13,7 +13,6 @@ pub struct Window {
 }
 
 impl Window {
-    // TODO: fix this constructor
     pub fn new(w: usize, h: usize, pos: (usize, usize)) -> Window {
         Window {height: h, width: w, pos: pos, cursor: pos, vga: VGA::get_vga()}
     }
