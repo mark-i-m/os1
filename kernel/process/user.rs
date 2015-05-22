2b9871e337a753ba620d3c24c94daa1f11446f4e
Mark Mansi <mmansi@utexas.edu>
2015-05-21 21:57:46 -0500

    Added 'user' process

---
+++ kernel/process/user.rs
@@ -0,0 +1,25 @@
+// Will eventually be a user process, but there is a long way to go
+use super::Process;
+
+pub fn run(this: &Process) -> usize {
+    use super::super::window::{Window, Color};
+
+    // clear the screen
+    Window::clear_screen();
+
+    let mut w0 = Window::new(40, 10, (4, 10));
+
+    w0.set_bg_color(Color::LightBlue);
+    w0.paint();
+
+    w0.set_cursor((1, 1));
+    w0.set_bg_color(Color::LightGreen);
+    w0.set_fg_color(Color::Red);
+    w0.put_str("Hello World!");
+
+    w0.set_cursor((2,1));
+    w0.put_str("from ");
+    w0.put_str(this.name);
+
+    0
+}
