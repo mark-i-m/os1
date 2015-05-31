be34971c1fcda318bfcf6e863e87d6b87a4ee808
Mark Mansi <mmansi@utexas.edu>
2015-05-31 14:45:32 -0700

    Let init terminate naturally

--- kernel/process/init.rs
+++ kernel/process/init.rs
@@ -9,12 +9,14 @@ use core::option::Option::None;
 
 // The init process routine
 pub fn run(this: &Process) -> usize {
-
     printf!("init about to yield\n");
     //let p0 = Box::new(Process::new("p0", super::user::run));
     //ready_queue::make_ready(p0);
 
     unsafe{super::proc_yield(&None);}
 
-    panic!("Yay!");
+    let x = 3;
+    let y = 3;
+
+    0
 }
