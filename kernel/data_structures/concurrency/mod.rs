226acc3c5a42d20d34ac7bcf39eed18b9e639cdc
Mark Mansi <mmansi@utexas.edu>
2015-11-09 00:59:08 -0600

    Starting a Semaphore

--- kernel/data_structures/concurrency/mod.rs
+++ kernel/data_structures/concurrency/mod.rs
@@ -2,4 +2,8 @@
 
 pub use self::atomic32::Atomic32;
 
+pub use self::semaphore::Semaphore;
+
 mod atomic32;
+
+mod semaphore;
---
+++ kernel/data_structures/concurrency/semaphore.rs
@@ -0,0 +1,3 @@
+// A semaphore implementation based on the rust Mutex<T> type
+
+struct Semaphore;
