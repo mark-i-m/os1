120182ed6f1658b20eaa65e71636c1583b38132b
Mark Mansi <markm@cs.wisc.edu>
2018-04-04 17:08:00 -0500

    rustfmt

--- kernel/memory/heap.rs
+++ kernel/memory/heap.rs
@@ -15,7 +15,9 @@ pub struct KernelAllocator {
 
 impl KernelAllocator {
     pub const fn new() -> Self {
-        KernelAllocator { heap: RefCell::new(None) }
+        KernelAllocator {
+            heap: RefCell::new(None),
+        }
     }
 
     pub fn set_heap(&mut self, heap: Allocator) {
@@ -26,14 +28,22 @@ impl KernelAllocator {
 unsafe impl<'a> Alloc for &'a KernelAllocator {
     unsafe fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
         off();
-        let ret = self.heap.borrow_mut().as_mut().unwrap().malloc(layout.size(), layout.align());
+        let ret = self.heap
+            .borrow_mut()
+            .as_mut()
+            .unwrap()
+            .malloc(layout.size(), layout.align());
         on();
         Ok(ret)
     }
 
     unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
         off();
-        let ret = self.heap.borrow_mut().as_mut().unwrap().free(ptr, layout.size());
+        let ret = self.heap
+            .borrow_mut()
+            .as_mut()
+            .unwrap()
+            .free(ptr, layout.size());
         on();
         ret
     }
@@ -41,7 +51,7 @@ unsafe impl<'a> Alloc for &'a KernelAllocator {
 
 /// Initialize the kernel heap
 pub fn init(allocator: &mut KernelAllocator, start: usize, size: usize) {
-    let heap = unsafe {Allocator::new(start as *mut u8, size)};
+    let heap = unsafe { Allocator::new(start as *mut u8, size) };
     let free_size = heap.size();
 
     allocator.set_heap(heap);
